79c949c144c5d8a816f41569f7787399d77224c9
Amjad-addas <65742033+Amjad-addas@users.noreply.github.com>
2026-02-23 14:39:17 +0300

    Update main.js

--- src/controller/main.js
+++ src/controller/main.js
@@ -1,76 +1,261 @@
 const date=new Date()
 let day =date.getDate()
-let month=date.getMonth()
-const dm=month
+let month=date.getMonth()+1
 let year = date.getFullYear()
 const date1 = new Date('2025-01-24');
-const date2 = new Date(`${year}-${month+1}-${day}`);
+const date2 = new Date(`${year}-${month}-${day}`);
+const lastday  = new Date(year, month, 0).getDate();
 const differenceInMs =  date2-date1;
 const differenceInDays = Math.round( differenceInMs / (1000 * 60 * 60 * 24));
 const weeks = Math.floor(differenceInDays/7)
 let thisHoure = date.getHours()
 const hours = Math.floor( differenceInMs / (1000 * 60 * 60))+thisHoure
-let houre = 0
-let minute=0
-let thisMinute = date.getMinutes()
-let sec = date.getSeconds()
-let yy=2025
-let ny=0
-if(year>yy){
-ny=year-yy
-if(month>1){
-    ny=ny+1
-}else if(month==1){
-    if(day>24){
-        ny=ny+1
+let houre=10
+year=parseInt(differenceInDays/365.5)
+const thisMinute =date.getMinutes()
+let minute=1000000 ,sec=0
+
+if(month==1){
+
+        if(day==24){
+            if(thisHoure==16){
+            houre = 0
+
+            if(thisMinute>=44){
+                differenceInDays++
+                minute = 60-(thisMinute-44)
+            }
+            else if(thisMinute<44){
+                
+                minute = 60-(44-thisMinute)
+            }
+        }
+        else if(thisHoure>16){
+            houre = thisHoure-16
+
+            if(thisMinute>=44){
+                differenceInDays++
+                minute = 60-(thisMinute-44)
+            }
+            else if(thisMinute<44){
+                
+                minute = 60-(44-thisMinute)
+            }
+        }
+        else if(thisHoure<16){
+            houre = 24-(16-thisHoure)
+
+            if(thisMinute>=44){
+                differenceInDays++
+                minute = 60-(thisMinute-44)
+            }
+            else if(thisMinute<44){
+                
+                minute = 60-(44-thisMinute)
+            }
+        }
+    }
+
+    else if(day>24){
+            if(thisHoure==16){
+            houre = 0
+
+            if(thisMinute>=44){
+                
+                minute = 60-(thisMinute-44)
+            }
+            else if(thisMinute<44){
+                
+                minute = 60-(44-thisMinute)
+            }
+        }
+        else if(thisHoure>16){
+            houre = thisHoure-16
+
+            if(thisMinute>=44){
+                
+                minute = 60-(thisMinute-44)
+            }
+            else if(thisMinute<44){
+                
+                minute = 60-(44-thisMinute)
+            }
+        }
+        else if(thisHoure<16){
+            houre = 24-(16-thisHoure)
+
+            if(thisMinute>=44){
+                
+                minute = 60-(thisMinute-44)
+            }
+            else if(thisMinute<44){
+                
+                minute = 60-(44-thisMinute)
+            }
+        }
+    }
+    else if(day<24){
+            if(thisHoure==16){
+            houre = 0
+
+            if(thisMinute>=44){
+                
+                minute = 60-(thisMinute-44)
+            }
+            else if(thisMinute<44){
+                
+                minute = 60-(44-thisMinute)
+            }
+        }
+        else if(thisHoure>16){
+            houre = thisHoure-16
+
+            if(thisMinute>=44){
+                
+                minute = 60-(thisMinute-44)
+            }
+            else if(thisMinute<44){
+                
+                minute = 60-(44-thisMinute)
+            }
+        }
+        else if(thisHoure<16){
+            houre = 24-(16-thisHoure)
+
+            if(thisMinute>=44){
+                
+                minute = 60-(thisMinute-44)
+            }
+            else if(thisMinute<44){
+                
+                minute = 60-(44-thisMinute)
+            }
+        }
     }
 }
-}else{
-    ny=0
-}
-console.log(day)
-if(day>=24){
-    day=day-24
-}else{
-    if(month==0)
-    month=12
-    month=month
-    if(dm%2==0)
-    day=30-(24-day)
-    else
-    day = 31-(24-day)
-}
-if(thisHoure>16){
-        day=day
-        houre=thisHoure-17
-        if(thisMinute>44){
-            minute=thisMinute-44
-            houre++
-        }else{
-            minute=16+thisMinute
-        }
-}
-else if(thisHoure==16){
-        if(thisMinute>=44){
-            houre=thisHoure-15
-            minute=thisMinute-44
-        }else{
-            day=day-1
-            minute=16+thisMinute
-            houre=thisHoure-16
 
+else if(month>1){
+
+    if(day==24){
+        month--
+        day=0
+            if(thisHoure==16){
+            houre = 0
+
+            if(thisMinute>=44){
+                differenceInDays++
+                minute = 60-(thisMinute-44)
+            }
+            else if(thisMinute<44){
+                
+                minute = 60-(44-thisMinute)
+            }
         }
-}
-else{
-    day=day-1
-            if(thisMinute>44){
-            minute=thisMinute-44
-            houre++
-        }else{
-            minute=16+thisMinute
-            houre=24-(16-thisHoure)
+        else if(thisHoure>16){
+            houre = thisHoure-16
+
+            if(thisMinute>=44){
+                differenceInDays++
+                minute = 60-(thisMinute-44)
+            }
+            else if(thisMinute<44){
+                
+                minute = 60-(44-thisMinute)
+            }
         }
+        else if(thisHoure<16){
+            houre = 24-(16-thisHoure)
+
+            if(thisMinute>=44){
+                differenceInDays++
+                minute = 60-(thisMinute-44)
+            }
+            else if(thisMinute<44){
+                
+                minute = 60-(44-thisMinute)
+            }
+        }
+    }
+
+    else if(day>24){
+        day=day-24
+            if(thisHoure==16){
+            houre = 0
+
+            if(thisMinute>=44){
+                
+                minute = 60-(thisMinute-44)
+            }
+            else if(thisMinute<44){
+                
+                minute = 60-(44-thisMinute)
+            }
+        }
+        else if(thisHoure>16){
+            houre = thisHoure-16
+
+            if(thisMinute>=44){
+                
+                minute = 60-(thisMinute-44)
+            }
+            else if(thisMinute<44){
+                
+                minute = 60-(44-thisMinute)
+            }
+        }
+        else if(thisHoure<16){
+            houre = 24-(16-thisHoure)
+
+            if(thisMinute>=44){
+                
+                minute = 60-(thisMinute-44)
+            }
+            else if(thisMinute<44){
+                
+                minute = 60-(44-thisMinute)
+            }
+        }
+    }
+    else if(day<24){
+        month--
+        day = lastday-(24-day)
+            if(thisHoure==16){
+            houre = 0
+
+            if(thisMinute>=44){
+                
+                minute = 60-(thisMinute-44)
+            }
+            else if(thisMinute<44){
+                
+                minute = 60-(44-thisMinute)
+            }
+        }
+        else if(thisHoure>16){
+            houre = thisHoure-16
+
+            if(thisMinute>=44){
+                
+                minute = 60-(thisMinute-44)
+            }
+            else if(thisMinute<44){
+                
+                minute = 60-(44-thisMinute)
+            }
+        }
+        else if(thisHoure<16){
+            houre = 24-(16-thisHoure)
+
+            if(thisMinute>=44){
+                
+                minute = 60-(thisMinute-44)
+            }
+            else if(thisMinute<44){
+                
+                minute = 60-(44-thisMinute)
+            }
+        }
+    }
 }
 
-year=ny
 export  {month,year,day,differenceInDays,weeks,hours,thisHoure,thisMinute,houre,minute,sec}
